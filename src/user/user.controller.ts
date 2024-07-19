73ca3f744220db14abc42a1ed7fd6457f86cdf8b
posh101 <paulorife@gmail.com>
2024-07-19 07:51:42 -0700

    my first commit

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { UserService } from '../user/user.service';
+import { User } from '../entities/user.entity';
 
 @Controller('api/v1/user')
 export class UserController {
-
-    @Get(':id')
-    async getUserDetails(@Param('id')userId: string) {
-       return this.UserService.getDetails(userId)
-    }
+  @Get(':id')
+  async findOne(@Param('id') userId: string) {
+    return this.UserService.findOne(id);
+  }
 }
