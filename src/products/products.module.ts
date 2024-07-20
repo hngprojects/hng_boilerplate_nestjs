601d918077c227db2a47b890b898402e9f20c33b
DCTP-BE <akpata.thelma@gmail.com>
2024-07-20 15:06:22 +0100

    product controller update

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { ProductsController } from './products.controller';
 
 @Module({
-  providers: [ProductsService]
+  providers: [ProductsService],
+  controllers: [ProductsController]
 })
 export class ProductsModule {}
