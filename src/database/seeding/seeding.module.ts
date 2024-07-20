0f573b0f5d3161626fc846b7af0b44af04468e24
Ayomide <ilesanmibry@gmail.com>
2024-07-20 12:02:58 +0100

    chore: database schema and typeorm setup

--- src/database/seeding/seeding.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SeedingService } from './seeding.service';
-// import { SeedingController } from './seeding.controller';
-
-@Module({
-  providers: [SeedingService],
-})
-export class SeedingModule {}
