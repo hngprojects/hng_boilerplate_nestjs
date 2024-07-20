6b6ae0a6c6385c2069c87e3396661c9b89135602
DCTP-BE <akpata.thelma@gmail.com>
2024-07-20 15:29:54 +0100

    product entity update

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,20 @@
+
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column('text')
+  description: string;
+
+  @Column()
+  category: string;
+
+  @Column('text', { array: true })
+  tags: string[];
+}
