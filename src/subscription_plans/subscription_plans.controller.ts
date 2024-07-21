aae5507e46a4b2683a939900d44ea28b14dd87d2
Dave <davidolanrewaju538@gmail.com>
2024-07-22 00:01:42 +0100

    Update subscription_plans.controller.ts

--- src/subscription_plans/subscription_plans.controller.ts
+++ src/subscription_plans/subscription_plans.controller.ts
@@ -4,7 +4,7 @@ import {
   Body,
 } from '@nestjs/common';
 import { SubscriptionPlansService } from './subscription_plans.service';
-import { CreateSubscriptionPlanDto } from './dto/create-subscription_plan.dto;
+import { CreateSubscriptionPlanDto } from 'src/subscription_plans/dto/create-subscription_plan.dto;
 
 @Controller('subscription-plans')
 export class SubscriptionPlansController {
