19a2256bee9cf8fd8285a9cbfce2df26187ee059
King-Mikaelson <anazodomichael27@gmail.com>
2024-07-21 22:05:41 +0100

    feat(user): fixed bugs in user spec.ts files

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -7,7 +7,7 @@ dotenv.config();
 const isDevelopment = process.env.NODE_ENV === 'development';
 
 const dataSource = new DataSource({
-  type: (process.env.DB_TYPE as 'postgres') || 'postgres',
+  type: 'postgres',
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
