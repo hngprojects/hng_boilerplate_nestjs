d2fc0d5e28b9b40cac12316ab9a79da07a656317
Dave <davidolanrewaju538@gmail.com>
2024-07-21 23:56:53 +0100

    Update subscription_plan.entity.ts

--- src/database/entities/subscription_plan.entity.ts
+++ src/database/entities/subscription_plan.entity.ts
@@ -1,4 +1,4 @@
-import { Feature } from 'src/features/entities/feature.entity';
+import { Feature } from 'src/database/entities/feature.entity';
 import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity()
