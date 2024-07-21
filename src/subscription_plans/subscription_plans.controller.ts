46b91b038f482dc1b0f647201044cb7c0d438c1a
Dave <davidolanrewaju538@gmail.com>
2024-07-21 23:54:05 +0100

    Update subscription_plans.controller.ts

--- src/subscription_plans/subscription_plans.controller.ts
+++ src/subscription_plans/subscription_plans.controller.ts
@@ -1,15 +1,10 @@
 import {
   Controller,
-  Get,
   Post,
   Body,
-  // Patch,
-  Param,
-  // Delete,
 } from '@nestjs/common';
 import { SubscriptionPlansService } from './subscription_plans.service';
-import { CreateSubscriptionPlanDto } from './dto/create-subscription_plan.dto';
-// import { UpdateSubscriptionPlanDto } from './dto/update-subscription_plan.dto';
+import { CreateSubscriptionPlanDto } from './dto/create-subscription_plan.dto;
 
 @Controller('subscription-plans')
 export class SubscriptionPlansController {
@@ -19,27 +14,4 @@ export class SubscriptionPlansController {
   create(@Body() createSubscriptionPlanDto: CreateSubscriptionPlanDto) {
     return this.subscriptionPlansService.create(createSubscriptionPlanDto);
   }
-
-  @Get()
-  findAll() {
-    return this.subscriptionPlansService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.subscriptionPlansService.findOne(id);
-  }
-
-  // @Patch(':id')
-  // update(
-  //   @Param('id') id: string,
-  //   @Body() updateSubscriptionPlanDto: UpdateSubscriptionPlanDto,
-  // ) {
-  //   return this.subscriptionPlansService.update(+id, updateSubscriptionPlanDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.subscriptionPlansService.remove(+id);
-  // }
 }
