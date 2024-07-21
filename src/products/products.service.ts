5866083303ebc2c9801f335c96929cabdbd04ae2
DCTP-BE <akpata.thelma@gmail.com>
2024-07-21 10:38:07 +0100

    roduct.name fix

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -15,7 +15,7 @@ export class ProductsService {
 
     const [results, total] = await this.productsRepository
       .createQueryBuilder('product')
-      .where("product.fullTextSearch @@ plainto_tsquery('english', :query)", { query })
+      .where("to_tsvector('english', product.name) @@ plainto_tsquery('english', :query) OR to_tsvector('english', product.description) @@ plainto_tsquery('english', :query) OR to_tsvector('english', product.category) @@ plainto_tsquery('english', :query) OR to_tsvector('english', array_to_string(product.tags, ' ')) @@ plainto_tsquery('english', :query)", { query })
       .skip(skip)
       .take(limit)
       .getManyAndCount();
@@ -23,54 +23,3 @@ export class ProductsService {
     return { total, results };
   }
 }
-
-
-// import { Injectable } from '@nestjs/common';
-// import { InjectRepository } from '@nestjs/typeorm';
-// import { Repository } from 'typeorm';
-// import { Product } from './entities/product.entity';
-
-// @Injectable()
-// export class ProductsService {
-//   constructor(
-//     @InjectRepository(Product)
-//     private productsRepository: Repository<Product>,
-//   ) {}
-
-//   async searchProducts(query: string, page: number = 1, limit: number = 10): Promise<{ total: number; results: Product[] }> {
-//     const skip = (page - 1) * limit;
-
-//     const [results, total] = await this.productsRepository
-//     .createQueryBuilder('product')
-//     .where("product.fullTextSearch @@ plainto_tsquery('english', :query)", { query })
-//     .skip(skip)
-//     .take(limit)
-//     .getManyAndCount();
-
-//     return { total, results };
-//   }
-// }
-
-
-
-
-// import { Injectable } from '@nestjs/common';
-
-// @Injectable()
-// export class ProductsService {
-//   constructor(
-//     @InjectRepository(Product)
-//     private productRepository: Repository<Product>,
-//   ) {}
-
-//   async search(query: string, page: number = 1, limit: number = 10): Promise<{ results: Product[], total: number }> {
-//     const [results, total] = await this.productRepository
-//       .createQueryBuilder('product')
-//       .where(`to_tsvector('english', product.name || ' ' || product.description || ' ' || product.category || ' ' || array_to_string(product.tags, ' ')) @@ plainto_tsquery('english', :query)`, { query })
-//       .skip((page - 1) * limit)
-//       .take(limit)
-//       .getManyAndCount();
-
-//     return { results, total };
-//   }
-// }   
