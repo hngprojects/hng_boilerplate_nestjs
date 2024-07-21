ddc9368374dbde3b3cc3a64805c1c03845921db4
Dave <davidolanrewaju538@gmail.com>
2024-07-21 23:58:04 +0100

    Update features.module.ts

--- src/features/features.module.ts
+++ src/features/features.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { FeaturesService } from './features.service';
 import { FeaturesController } from './features.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Feature } from '../entities/feature.entity';
+import { Feature } from 'src/database/entities/feature.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Feature])],
