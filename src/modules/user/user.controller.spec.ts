49407a6ac64c2e653fcd42e6e45071077098a919
King-Mikaelson <anazodomichael27@gmail.com>
2024-07-21 21:55:43 +0100

    feat(user): added user modules and getUser routes

--- src/modules/user/user.controller.spec.ts
+++ src/modules/user/user.controller.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
 
 describe('UserController', () => {
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        JwtModule.register({
+          secret: process.env.JWT_SECRET, // Use environment variables for security
+          signOptions: { expiresIn: '60m' }, // Token expiration time
+        }),
+        TypeOrmModule.forFeature([User]),
+      ],
       controllers: [UserController],
       providers: [UserService],
     }).compile();
--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../../entities/user.entity';
 
 describe('UserService', () => {
   let service: UserService;
@@ -7,6 +10,13 @@ describe('UserService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [UserService],
+      imports: [
+        JwtModule.register({
+          secret: process.env.JWT_SECRET, // Use environment variables for security
+          signOptions: { expiresIn: '60m' }, // Token expiration time
+        }),
+        TypeOrmModule.forFeature([User]),
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
