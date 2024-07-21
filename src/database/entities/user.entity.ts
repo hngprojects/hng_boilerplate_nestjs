e2847e05baa661f279da7182fcc43b4c41ca0adb
Ayomide <ilesanmibry@gmail.com>
2024-07-22 00:44:59 +0100

    chore: updated entities files in db

--- src/database/entities/user.entity.ts
+++ src/database/entities/user.entity.ts
@@ -6,8 +6,6 @@ import {
   UpdateDateColumn,
   OneToMany,
   ManyToOne,
-  ManyToMany,
-  JoinTable,
 } from 'typeorm';
 import { Testimonial } from './testimonials.entity';
 import { Review } from './reviews.entity';
@@ -15,8 +13,19 @@ import { Sms } from './sms.entity';
 import { JobListing } from './job-listing.entity';
 import { Organisation } from './org.entity';
 import { Invite } from './invite.entity';
+import { AboutUsAccessToken } from './about-us-access-token.entity';
+import { HelpCenterTopic } from './help-center-topic.entity';
+import { NotificationSettings } from './notification-settings.entity';
+import { Product } from './products.entity';
+import { Profile } from './profile.entity';
 import { Sessions } from './sessions.entity';
 import { Roles } from './roles.entity';
+
+export enum UserType {
+  SUPER_ADMIN = 'super_admin',
+  ADMIN = 'admin',
+  USER = 'vendor',
+}
 
 @Entity()
 export class User {
@@ -35,20 +44,18 @@ export class User {
   @Column({ nullable: false })
   password: string;
 
-  @OneToMany(() => Sessions, session => session.user)
-  sessions: Sessions[];
-
-  @Column({ nullable: false, default: false })
-  is2faEnabled: boolean;
+  @Column({
+    type: 'enum',
+    enum: UserType,
+    default: UserType.USER,
+  })
+  userType: UserType;
 
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
 
   @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
-
-  @ManyToOne(() => Roles, roles => roles.users)
-  role: Roles;
 
   @OneToMany(() => Testimonial, testimonial => testimonial.user)
   testimonials: Testimonial[];
@@ -62,10 +69,6 @@ export class User {
   @OneToMany(() => JobListing, jobListing => jobListing.user)
   jobListings: JobListing[];
 
-  @ManyToMany(() => Organisation)
-  @JoinTable()
-  organisations: Organisation[];
-
   @OneToMany(() => Organisation, organisation => organisation.owner)
   ownedOrganisations: Organisation[];
 
@@ -74,4 +77,25 @@ export class User {
 
   @OneToMany(() => Invite, invite => invite.user)
   invites: Invite[];
+
+  @OneToMany(() => Sessions, session => session.user)
+  sessions: Sessions[];
+
+  @OneToMany(() => AboutUsAccessToken, aboutUsAccessToken => aboutUsAccessToken.user)
+  aboutUsAccessTokens: AboutUsAccessToken[];
+
+  @OneToMany(() => HelpCenterTopic, topic => topic.user)
+  topics: HelpCenterTopic[];
+
+  @OneToMany(() => NotificationSettings, notificationSettings => notificationSettings.user)
+  notificationSettings: NotificationSettings[];
+
+  @OneToMany(() => Product, product => product.user)
+  products: Product[];
+
+  @OneToMany(() => Profile, profile => profile.user)
+  profile: Profile[];
+
+  @ManyToOne(() => Roles, roles => roles.users)
+  roles: Roles;
 }
