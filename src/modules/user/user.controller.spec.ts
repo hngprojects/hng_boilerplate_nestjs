083c49effc51473dd929c0daf103b82e979cff15
King-Mikaelson <anazodomichael27@gmail.com>
2024-07-21 22:02:08 +0100

    feat(user): fixed bugs in user spec.ts files

--- src/modules/user/user.controller.spec.ts
+++ src/modules/user/user.controller.spec.ts
@@ -3,7 +3,7 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/entities/user.entity';
+import { User } from '../../entities/user.entity';
 import dataSource from '../../database/data-source';
 
 describe('UserController', () => {
