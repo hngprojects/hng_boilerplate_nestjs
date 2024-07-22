065054d197cf26fdce777d46b2c2045c02dfe9b1
elijah arhinful <elijaharhinful8@gmail.com>
2024-07-22 10:27:06 +0000

    fix: resolved test issues

--- src/modules/blogs/blogs.controller.spec.ts
+++ src/modules/blogs/blogs.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BlogsController } from './blogs.controller';
 import { BlogsService } from './blogs.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Blog } from './entities/blog.entity';
 
 describe('BlogsController', () => {
   let controller: BlogsController;
@@ -8,7 +10,16 @@ describe('BlogsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BlogsController],
-      providers: [BlogsService],
+      providers: [
+        BlogsService,
+        {
+          provide: getRepositoryToken(Blog),
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<BlogsController>(BlogsController);
--- src/modules/blogs/blogs.service.spec.ts
+++ src/modules/blogs/blogs.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BlogsService } from './blogs.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Blog } from './entities/blog.entity';
 
 describe('BlogsService', () => {
   let service: BlogsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BlogsService],
+      providers: [
+        BlogsService,
+        {
+          provide: getRepositoryToken(Blog),
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<BlogsService>(BlogsService);
