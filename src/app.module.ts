8921c1fa01c8812cdb74b3ebc2d5325a459337f6
Ismail Akintunde <homohaykins619@gmail.com>
2024-07-24 00:26:37 +0100

    feat: registered guard in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,7 @@ import HealthController from './health.controller';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import authConfig from '../config/auth.config';
+import { AuthGuard } from './guards/auth.guard';
 
 @Module({
   providers: [
@@ -25,6 +26,10 @@ import authConfig from '../config/auth.config';
           whitelist: true,
           forbidNonWhitelisted: true,
         }),
+    },
+    {
+      provide: 'APP_GUARD',
+      useClass: AuthGuard,
     },
   ],
   imports: [
