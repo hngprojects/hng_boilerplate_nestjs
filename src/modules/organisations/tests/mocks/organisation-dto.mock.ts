92b792ef4c7cec7d39c40cb67b46f283b9bee355
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-24 02:01:07 +0300

    feat: created an organisaiton mock

---
+++ src/modules/organisations/tests/mocks/organisation-dto.mock.ts
@@ -0,0 +1,14 @@
+import { OrganisationRequestDto } from '../../dto/organisation.dto';
+
+const OrgRequestMockdto: OrganisationRequestDto = {
+  name: 'John & Co',
+  description: 'An imports organisation',
+  email: 'johnCo@exmaple.com',
+  industry: 'Import',
+  type: 'General',
+  country: 'Nigeria',
+  address: 'Street 101 Building 26',
+  state: 'Lagos',
+};
+
+export { OrgRequestMockdto };
