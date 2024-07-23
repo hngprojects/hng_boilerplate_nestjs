bedad6f7149097370b4df47cb4cc5aaeb80f2a83
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-24 01:54:10 +0300

    feat: added create and emailExists services

---
+++ src/modules/organisations/organisations.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Organisation } from './entities/organisations.entity';
+import { OrganisationRequestDto } from './dto/organisation.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../user/entities/user.entity';
+import { OrganisationMapper } from './mapper/organisation.mapper';
+import { CreateOrganisationMapper } from './mapper/create-organisation.mapper';
+
+@Injectable()
+export class OrganisationsService {
+  constructor(
+    @InjectRepository(Organisation)
+    private readonly organisationRepository: Repository<Organisation>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) {}
+
+  async create(createOrganisationDto: OrganisationRequestDto, userId: string) {
+    const emailFound = await this.emailExsits(createOrganisationDto.email);
+    if (emailFound)
+      throw new UnprocessableEntityException({
+        status: 'Unprocessable entity exception',
+        message: 'Invalid organisation credentials',
+        status_code: 422,
+      });
+    const owner = await this.userRepository.findOne({
+      where: { id: userId },
+    });
+    if (!owner) {
+      throw new Error('Owner not found');
+    }
+    const mapNewOrganisation = CreateOrganisationMapper.mapToEntity(createOrganisationDto, owner);
+    const newOrganisation = this.organisationRepository.create({
+      ...mapNewOrganisation,
+    });
+    await this.organisationRepository.save(newOrganisation);
+    const mappedResponse = OrganisationMapper.mapToResponseFormat(newOrganisation);
+    return { status: 'success', message: 'organisation created successfully', data: mappedResponse };
+  }
+
+  async emailExsits(email: string): Promise<boolean> {
+    const emailFound = await this.organisationRepository.findBy({ email });
+    return emailFound?.length ? true : false;
+  }
+}
