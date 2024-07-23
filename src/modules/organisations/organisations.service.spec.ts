c330e4d73f48799a81483d36b7f686fe734f55ac
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-24 01:53:31 +0300

    test: added tests cases for organisation serive

---
+++ src/modules/organisations/organisations.service.spec.ts
@@ -0,0 +1,92 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { OrganisationsService } from './organisations.service';
+import { Repository } from 'typeorm';
+import { User } from '../user/entities/user.entity';
+import { Organisation } from './entities/organisations.entity';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { validate } from 'class-validator';
+import { orgMock } from './tests/mocks/organisation.mock';
+import { OrgRequestMockdto } from './tests/mocks/organisation-dto.mock';
+import { UserModule } from '../user/user.module';
+import { OrganisationsModule } from './organisations.module';
+import UserService from '../user/user.service';
+import { UnprocessableEntityException } from '@nestjs/common';
+
+describe('OrganisationsService', () => {
+  let service: OrganisationsService;
+  let userRepository: Repository<User>;
+  let organisationRepository: Repository<Organisation>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        OrganisationsService,
+        {
+          provide: getRepositoryToken(Organisation),
+          useValue: {
+            findBy: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {
+            findBy: jest.fn(),
+            find: jest.fn(),
+            findOne: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+    service = module.get<OrganisationsService>(OrganisationsService);
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
+    organisationRepository = module.get<Repository<Organisation>>(getRepositoryToken(Organisation));
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('it should create an organisation', () => {
+    beforeEach(async () => {
+      const errors = await validate(OrgRequestMockdto);
+      expect(errors).toHaveLength(0);
+    });
+
+    it('creation of an organisation', async () => {
+      jest.spyOn(organisationRepository, 'findBy').mockResolvedValue(null);
+      jest.spyOn(userRepository, 'findOne').mockResolvedValue({
+        ...orgMock.owner,
+      } as User);
+      jest.spyOn(organisationRepository, 'create').mockReturnValue(orgMock);
+      jest.spyOn(organisationRepository, 'save').mockResolvedValue({
+        ...orgMock,
+      });
+
+      const result = await service.create(OrgRequestMockdto, orgMock.owner.id);
+      expect(result.status).toEqual('success');
+      expect(result.message).toEqual('organisation created successfully');
+    });
+  });
+
+  describe('error for an exsiting email', () => {
+    beforeEach(async () => {
+      const errors = await validate(OrgRequestMockdto);
+      expect(errors).toHaveLength(0);
+    });
+
+    it('should throw an error if the email already exists', async () => {
+      jest.spyOn(organisationRepository, 'findBy').mockResolvedValue([orgMock]);
+      await expect(service.create(OrgRequestMockdto, orgMock.owner.id)).rejects.toThrow(
+        new UnprocessableEntityException({
+          status: 'Unprocessable entity exception',
+          message: 'Invalid organisation credentials',
+          status_code: 422,
+        })
+      );
+    });
+  });
+});
