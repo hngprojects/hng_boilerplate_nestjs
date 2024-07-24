62c69c30ace722d85882a8e17a148dcc5b6a0ad0
Akanni Emmanuel <akannie33@gmail.com>
2024-07-24 16:43:52 +0100

    refactor: update squeeze controller and service

--- src/modules/squeeze/squeeze.controller.ts
+++ src/modules/squeeze/squeeze.controller.ts
@@ -1,34 +1,15 @@
-import { Controller, Post, Body, BadRequestException, InternalServerErrorException } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { SqueezeService } from './squeeze.service';
 import { CreateSqueezeDto } from './dto/create-squeeze.dto';
-import { plainToClass } from 'class-transformer';
-import { validate } from 'class-validator';
+import { skipAuth } from '../../helpers/skipAuth';
 
 @Controller('squeeze')
 export class SqueezeController {
   constructor(private readonly squeezeService: SqueezeService) {}
 
+  @skipAuth()
   @Post()
   async create(@Body() createSqueezeDto: CreateSqueezeDto) {
-    const dto = plainToClass(CreateSqueezeDto, createSqueezeDto);
-
-    const errors = await validate(dto);
-    if (errors.length > 0) {
-      throw new BadRequestException({
-        message: 'Failed to submit your request',
-        status_code: 400,
-      });
-    }
-
-    try {
-      const _ = await this.squeezeService.create(dto);
-      return { message: 'Your request has been received. You will get a template shortly.' };
-    } catch (err) {
-      if (err.code === '23505') {
-        throw new BadRequestException('Duplicate entry for email');
-      }
-
-      throw new InternalServerErrorException('Failed to create squeeze');
-    }
+    return await this.squeezeService.create(createSqueezeDto);
   }
 }
--- src/modules/squeeze/squeeze.service.ts
+++ src/modules/squeeze/squeeze.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { CreateSqueezeDto } from './dto/create-squeeze.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -11,9 +11,26 @@ export class SqueezeService {
     private squeezeRepository: Repository<Squeeze>
   ) {}
 
-  async create(createSqueezeDto: CreateSqueezeDto): Promise<Squeeze> {
+  async create(createSqueezeDto: CreateSqueezeDto) {
+    const exists = await this.emailExists(createSqueezeDto.email);
+    if (exists) {
+      throw new UnprocessableEntityException({
+        message: 'Squeeze information exists for the provided email',
+        status_code: 422,
+      });
+    }
     const data = this.squeezeRepository.create(createSqueezeDto);
 
-    return this.squeezeRepository.save(data);
+    await this.squeezeRepository.save(data);
+
+    return {
+      status_code: 201,
+      message: 'Your request has been received. You will get a template shortly.',
+    };
+  }
+
+  async emailExists(email: string): Promise<boolean> {
+    const emailFound = await this.squeezeRepository.findBy({ email });
+    return emailFound?.length ? true : false;
   }
 }
