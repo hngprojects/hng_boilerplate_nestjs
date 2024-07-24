713ddca7dd0c4f7b00d8ab446387e907226cc775
King-Mikaelson <anazodomichael27@gmail.com>
2024-07-24 12:21:41 +0100

    feat(email-service): renamed interface name

--- src/modules/email/email.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { EmailService } from './email.service';
-
-@Controller('email')
-export class EmailController {
-  constructor(private readonly emailService: EmailService) {}
-}
--- src/modules/email/email.module.ts
+++ src/modules/email/email.module.ts
@@ -1,11 +1,9 @@
 // src/email/email.module.ts
 import { Module } from '@nestjs/common';
-import { EmailController } from './email.controller';
 import { EmailService } from './email.service';
 
 @Module({
   providers: [EmailService],
   exports: [EmailService],
-  controllers: [EmailController],
 })
 export class EmailModule {}
