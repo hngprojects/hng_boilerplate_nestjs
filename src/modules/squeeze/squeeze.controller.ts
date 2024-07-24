5ea475720ef96f6dd774785bf37d69a4f6aa2280
DevNickjr <nicholasduadei14@gmail.com>
2024-07-24 23:48:26 +0100

    refactor: skip auth and update docs

--- src/modules/squeeze/squeeze.controller.ts
+++ src/modules/squeeze/squeeze.controller.ts
@@ -1,13 +1,16 @@
 import { Body, Controller, Post, Request } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { SqueezeService } from './squeeze.service';
 import { SqueezeRequestDto } from './dto/squeeze.dto';
+import { skipAuth } from 'src/helpers/skipAuth';
 
 @ApiTags('Squeeze')
 @Controller('squeeze')
 export class SqueezeController {
   constructor(private readonly SqueezeService: SqueezeService) {}
 
+  @ApiOperation({ summary: `Create squeeze record` })
+  @skipAuth()
   @Post('/')
   async create(@Body() createSqueezeDto: SqueezeRequestDto, @Request() req) {
     return this.SqueezeService.create(createSqueezeDto);
