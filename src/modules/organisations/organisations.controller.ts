12e771f9c93b09375c2f5bdc131f7c916bc77215
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-07-24 19:27:57 +0000

    feat: creating remove user endpoint

--- src/modules/organisations/organisations.controller.ts
+++ src/modules/organisations/organisations.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Request, Param, Delete } from '@nestjs/common';
 import { OrganisationsService } from './organisations.service';
 import { OrganisationRequestDto } from './dto/organisation.dto';
 import { AuthGuard } from '@nestjs/passport';
@@ -11,5 +11,11 @@ export class OrganisationsController {
   async create(@Body() createOrganisationDto: OrganisationRequestDto, @Request() req) {
     const user = req['user'];
     return this.organisationsService.create(createOrganisationDto, user.sub);
+  }
+
+  @Delete('/:org_id/users/:user_id')
+  async removeUser(@Param('org_id') orgId: string, @Param('user_id') userId: string, @Request() req) {
+    const currentUser = req['user'];
+    return this.organisationsService.removeUser(orgId, userId, currentUser.sub);
   }
 }
