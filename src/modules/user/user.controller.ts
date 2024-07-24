bc377319094705de7ba1cfb6359f446402be1348
elijah arhinful <elijaharhinful8@gmail.com>
2024-07-24 18:38:09 +0000

    chore: added documentation

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,11 +2,20 @@ import { Controller, Patch, Param, Body, UsePipes, ValidationPipe, Request } fro
 import UserService from './user.service';
 import { UpdateUserDto } from './dto/update-user-dto';
 import { UserPayload } from './interfaces/user-payload.interface';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiBearerAuth()
+@ApiTags('Users')
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiOperation({ summary: 'Update User' })
+  @ApiResponse({
+    status: 200,
+    description: 'User updated seuccessfully',
+    type: UpdateUserDto,
+  })
   @Patch(':userId')
   async updateUser(
     @Request() req: { user: UserPayload },
