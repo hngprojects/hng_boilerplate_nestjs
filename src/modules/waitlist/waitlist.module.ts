7c02f7130db9c053096182a955e44113da53cd53
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2024-07-24 19:14:02 +0100

    feat: implemented the waitlist endpoint

--- src/modules/waitlist/waitlist.module.ts
+++ src/modules/waitlist/waitlist.module.ts
@@ -3,10 +3,11 @@ import { WaitlistController } from './waitlist.controller';
 import { WaitlistService } from './waitlist.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Waitlist } from './waitlist.entity';
+import { EmailService } from '../email/email.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Waitlist])],
   controllers: [WaitlistController],
-  providers: [WaitlistService],
+  providers: [WaitlistService, EmailService],
 })
 export class WaitlistModule {}
--- src/modules/waitlist/waitlist.service.ts
+++ src/modules/waitlist/waitlist.service.ts
@@ -3,7 +3,6 @@ import { CreateWaitlistUserDto } from './dto/create-waitlist-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Waitlist } from './waitlist.entity';
 import { Repository } from 'typeorm';
-import * as nodemailer from 'nodemailer';
 import * as dotenv from 'dotenv';
 import { EMAIL_EXISTS, WAITLIST_USER_CREATED_SUCCESSFULLY } from 'src/helpers/SystemMessages';
 import { EmailService } from '../email/email.service';
