45e722e8051452bc2522e45308bf0061367b8e9f
ZEDD2468 <victoradebayo360@gmail.com>
2024-07-24 19:30:41 +0100

    chore: cpmpleted

--- src/modules/timezones/timezones.controller.ts
+++ src/modules/timezones/timezones.controller.ts
@@ -3,21 +3,24 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { TimezonesService } from './timezones.service';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { CreateTimezoneDto } from './dto/create-timezone.dto';
-// import { AuthGuard } from '../auth/auth.guard';
 
 @ApiTags('Timezones')
 @Controller('timezones')
 export class TimezonesController {
   constructor(private readonly timezonesService: TimezonesService) {}
 
-  // Without authentication
   @Get()
   @ApiOperation({ summary: 'Get supported timezones without authentication' })
   @ApiResponse({ status: 200, description: 'Successfully fetched timezones.' })
-  async getTimezonesWithoutAuth(): Promise<{ status: string; data: { timezones: CreateTimezoneDto[] } }> {
+  async getTimezonesWithoutAuth(): Promise<{
+    status: string;
+    status_code: number;
+    data: { timezones: CreateTimezoneDto[] };
+  }> {
     const timezones = await this.timezonesService.getSupportedTimezones();
     return {
       status: 'success',
+      status_code: 200,
       data: {
         timezones,
       },
