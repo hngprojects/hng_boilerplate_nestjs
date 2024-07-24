b43ba4c29e33cfe11913dc910099b58a3719a6cb
JohnKamaujk <johnjkkamau@gmail.com>
2024-07-25 00:16:47 +0300

    chore: login response DTO

--- src/modules/auth/dto/login-response.dto.ts
+++ src/modules/auth/dto/login-response.dto.ts
@@ -6,7 +6,17 @@ export class LoginResponseDto {
       first_name: string;
       last_name: string;
       email: string;
+      last_login_device: {
+        device_browser: string;
+        device_browser_version: string;
+        device_os: string;
+        device_os_version: string;
+        device_type: string;
+        device_brand: string;
+        device_model: string;
+      };
     };
+    session_id: string;
   };
   access_token: string;
 }
