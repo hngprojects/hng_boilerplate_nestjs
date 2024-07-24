aed449ecb03ce09d6ab42d95467af7226dc70238
Osarugue Enehizena <nehirugue@gmail.com>
2024-07-24 08:07:05 +0100

    chore(database): setup done

---
+++ db/migrations/1721802019252-migration.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Migration1721802019252 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
