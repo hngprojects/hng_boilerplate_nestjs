98f37ef3c5ee476748081fd196bc7daa8eb98087
ZEDD2468 <victoradebayo360@gmail.com>
2024-07-25 07:39:57 +0100

    feat(timezones): created timezone endpoint

--- src/helpers/SystemMessages.ts
+++ src/helpers/SystemMessages.ts
@@ -4,7 +4,7 @@ export const ERROR_OCCURED = 'Error Occured Performing this request';
 export const USER_ACCOUNT_EXIST = 'Account with the specified email exists';
 export const UNAUTHENTICATED_MESSAGE = 'User is currently unauthorized, kindly authenticate to continue';
 
-export const TIMEZONE_CREATED_SUCCESSFULLY = 'Time Created Successfully';
-export const FETCH_TIMEZONE_FAILURE = 'Error Occured while creating user, kindly try again';
+export const TIMEZONE_CREATED_SUCCESSFULLY = 'Timezone created Successfully';
+export const FETCH_TIMEZONE_FAILURE = 'Error Occured while creating Timezone, kindly try again';
 export const SUCCESS = 'Timezone fetched successfully';
-export const TIMEZONE_ALREADY_EXISTS = 'Account with the specified email exists';
+export const TIMEZONE_ALREADY_EXISTS = 'Timezone already exists';
