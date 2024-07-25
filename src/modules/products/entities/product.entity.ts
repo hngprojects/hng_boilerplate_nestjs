53af713d8c79c48eacc4056a526c0dcbba88d2ab
Leona-cloud <chisomokafor038@gmail.com>
2024-07-25 16:45:23 +0100

    chore: fix merge conflicts

--- src/modules/products/entities/product.entity.ts
+++ src/modules/products/entities/product.entity.ts
@@ -1,24 +1,19 @@
 import { ProductCategory } from '../../../modules/product-category/entities/product-category.entity';
 import { AbstractBaseEntity } from '../../../entities/base.entity';
 import { User } from '../../../modules/user/entities/user.entity';
-import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
 
 @Entity()
 export class Product extends AbstractBaseEntity {
-  @ApiProperty()
   @Column({ type: 'text' })
   product_name: string;
 
-  @ApiProperty()
   @Column('text')
   description: string;
 
-  @ApiProperty()
   @Column('int')
   quantity: number;
 
-  @ApiProperty()
   @Column('int')
   price: number;
 
