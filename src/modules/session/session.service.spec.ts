ba4861a484f97e0c0cd0c50daadeb8b2d9c1fac2
JohnKamaujk <johnjkkamau@gmail.com>
2024-07-25 19:59:35 +0300

    test: session service unit tests

--- src/modules/session/session.service.spec.ts
+++ src/modules/session/session.service.spec.ts
@@ -1,8 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { SessionService } from './session.service'; // Adjust the import as needed
-import { Session } from './entities/session.entity'; // Adjust the import as needed
+import { SessionService } from './session.service';
+import { Session } from './entities/session.entity';
 
 describe('SessionService', () => {
   let service: SessionService;
