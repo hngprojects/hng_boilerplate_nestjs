330fab97fbb2a1d9419430c9dc7a140a0e762066
Oluwaseyi Idowu <idowuseyi22@gmail.com>
2024-07-25 02:06:28 +0100

    fix: conflict in user.controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,11 +1,48 @@
-import { Controller, Param, Delete, BadRequestException, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Patch,
+  Param,
+  Delete,
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Body,
+  UsePipes,
+  ValidationPipe,
+  Request,
+} from '@nestjs/common';
 import UserService from './user.service';
 import { UpdateUserDto } from './dto/update-user-dto';
+import { UserPayload } from './interfaces/user-payload.interface';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiBearerAuth()
+@ApiTags('Users')
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiOperation({ summary: 'Update User' })
+  @ApiResponse({
+    status: 200,
+    description: 'User updated seuccessfully',
+    type: UpdateUserDto,
+  })
+  @Patch(':userId')
+  async updateUser(
+    @Request() req: { user: UserPayload },
+    @Param('userId') userId: string,
+    @Body() updatedUserDto: UpdateUserDto
+  ) {
+    return this.userService.updateUser(userId, updatedUserDto, req.user);
+  }
+
+  @ApiOperation({ summary: 'Delete User' })
+  @ApiResponse({
+    status: 200,
+    description: 'User deleted successfully',
+    type: UpdateUserDto,
+  })
   @Delete(':user_id')
   async softDelete(
     @Param('user_id') user_id: string,
