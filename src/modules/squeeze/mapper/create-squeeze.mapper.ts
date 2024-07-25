e60952122c2b3b106f1502dc4fe2012b8ccd0c58
DevNickjr <nicholasduadei14@gmail.com>
2024-07-25 08:05:43 +0100

    refactor: clean up mapper

--- src/modules/squeeze/mapper/create-squeeze.mapper.ts
+++ src/modules/squeeze/mapper/create-squeeze.mapper.ts
@@ -6,17 +6,6 @@ export class CreateSqueezeMapper {
     if (!dto) {
       throw new Error('SqueezeRequestDto is required');
     }
-
-    const squeeze = new Squeeze();
-    squeeze.company = dto.company;
-    squeeze.email = dto.email;
-    squeeze.first_name = dto.first_name;
-    squeeze.interests = dto.interests;
-    squeeze.job_title = dto.job_title;
-    squeeze.location = dto.location;
-    squeeze.phone = dto.phone;
-    squeeze.referral_source = dto.referral_source;
-
-    return squeeze;
+    return Object.assign(new Squeeze(), dto);
   }
 }
