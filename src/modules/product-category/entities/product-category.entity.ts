c7842f652399e68885e9f6ed5403cfc8f7b1417c
zee-ham-su <hsufiian@yahoo.com>
2024-07-25 16:59:16 +0000

    chore: Refactor product category and product entities

--- src/modules/product-category/entities/product-category.entity.ts
+++ src/modules/product-category/entities/product-category.entity.ts
@@ -1,21 +1,17 @@
 import { Entity, PrimaryGeneratedColumn, Column, Index, OneToMany } from 'typeorm';
-import { ApiProperty } from '@nestjs/swagger';
 import { AbstractBaseEntity } from '../../../entities/base.entity';
 import { Product } from '../../products/entities/product.entity';
 
 @Entity('categories')
 export class ProductCategory extends AbstractBaseEntity {
-  @ApiProperty()
   @Column({ length: 500 })
   @Index()
   name: string;
 
-  @ApiProperty()
   @Column('text')
   @Index()
   description: string;
 
-  @ApiProperty()
   @Column('text')
   slug: string;
 
