a1bff4bdc3ff4b30d7826c24932d1433382233eb
Osarugue Enehizena <nehirugue@gmail.com>
2024-07-26 11:27:11 +0100

    chore(branch name): updated branch name

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -8,14 +8,20 @@ import appConfig from '../../../config/auth.config';
 import { Repository } from 'typeorm';
 import AuthenticationService from './auth.service';
 import UserService from '../user/user.service';
-import { JwtStrategy } from './strategies/jwt.strategy';
+import { OtpModule } from '../otp/otp.module';
+import { EmailModule } from '../email/email.module';
+import { OtpService } from '../otp/otp.service';
+import { EmailService } from '../email/email.service';
+import { Otp } from '../otp/entities/otp.entity';
 
 @Module({
   controllers: [RegistrationController],
-  providers: [AuthenticationService, Repository, UserService, JwtStrategy],
+  providers: [AuthenticationService, Repository, UserService, OtpService, EmailService],
   imports: [
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([User, Otp]),
     PassportModule,
+    OtpModule,
+    EmailModule,
     JwtModule.register({
       global: true,
       secret: appConfig().jwtSecret,
