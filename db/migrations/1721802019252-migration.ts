fbc2b8c02b2c6308d6afe0804c4d056613298d0f
Osarugue Enehizena <nehirugue@gmail.com>
2024-07-26 13:24:31 +0100

    chore(migrations): removed migrations

--- db/migrations/1721802019252-migration.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class Migration1721802019252 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
