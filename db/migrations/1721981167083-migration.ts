e3bc26212cb0c908f82a55fae6893bd0b7d9b6b9
Osarugue Enehizena <nehirugue@gmail.com>
2024-07-26 10:10:50 +0100

    chore(Migration): Deleted generated local migrations

--- db/migrations/1721981167083-migration.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class Migration1721981167083 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
