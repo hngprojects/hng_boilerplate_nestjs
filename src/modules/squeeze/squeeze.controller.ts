f6a84dcb1722dedeaee90c2f58cab6702b5ca4b4
Akanni Emmanuel <akannie33@gmail.com>
2024-07-27 23:00:29 +0100

    refactor(squeeze): make controller method name descriptive

--- src/modules/squeeze/squeeze.controller.ts
+++ src/modules/squeeze/squeeze.controller.ts
@@ -30,7 +30,7 @@ export class SqueezeController {
     description: 'The squeeze page record can only be updated once.',
     status: 403,
   })
-  async update(@Body() updateDto: UpdateSqueezeDto) {
+  async updateSqueeze(@Body() updateDto: UpdateSqueezeDto) {
     const updatedSqueeze = await this.squeezeService.updateSqueeze(updateDto);
     return {
       message: 'Your record has been successfully updated. You cannot update it again.',
