9ab4ac4397ebaf07bfb2905d2066ed3a8c178bc6
akinwale <alexanderakinlami@gmail.com>
2024-07-28 00:45:28 +0100

    feat: made changes

--- src/modules/blog/entities/blog-comment.entity.ts
+++ src/modules/blog/entities/blog-comment.entity.ts
@@ -5,7 +5,6 @@ import { User } from '../../user/entities/user.entity';
 
 @Entity()
 export class BlogPostComment extends AbstractBaseEntity {
-  @PrimaryGeneratedColumn()
   @Column('text')
   content: string;
 
