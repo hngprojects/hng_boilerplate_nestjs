2491b6960f48b8d5b579263506b8b9790f12b936
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-07-30 23:04:54 +0000

    fix: adding descriptors to the organization member's entity

--- src/modules/organisations/entities/org-members.entity.ts
+++ src/modules/organisations/entities/org-members.entity.ts
@@ -1,18 +1,24 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Entity, Column, ManyToOne } from 'typeorm';
 import { AbstractBaseEntity } from './../../../entities/base.entity';
 import { User } from '../../user/entities/user.entity';
 import { Organisation } from './organisations.entity';
 import { Profile } from '../../profile/entities/profile.entity';
 
+export enum MemberRole {
+  ADMIN = 'admin',
+  MEMBER = 'member',
+  GUEST = 'guest',
+}
+
 @Entity()
 export class OrganisationMember extends AbstractBaseEntity {
-  @Column()
+  @Column({ nullable: false })
   user_id: string;
 
-  @Column()
+  @Column({ nullable: false })
   organisation_id: string;
 
-  @Column()
+  @Column({ nullable: false })
   profile_id: string;
 
   @ManyToOne(() => User, user => user.organisationMembers)
@@ -21,7 +27,11 @@ export class OrganisationMember extends AbstractBaseEntity {
   @ManyToOne(() => Organisation, organisation => organisation.organisationMembers)
   organisation: Organisation;
 
-  @Column()
+  @Column({
+    type: 'enum',
+    enum: MemberRole,
+    default: MemberRole.MEMBER,
+  })
   role: string;
 
   @ManyToOne(() => Profile)
