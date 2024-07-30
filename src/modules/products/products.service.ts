fedab5d6a98a3b20d1b3b6adf0323cd2e26a3847
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-30 03:42:32 +0300

    fix: product not saving org id

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -15,26 +15,33 @@ import { Organisation } from '../organisations/entities/organisations.entity';
 export class ProductsService {
   constructor(
     @InjectRepository(Product) private productRepository: Repository<Product>,
-    @InjectRepository(Product) private organisationRepository: Repository<Organisation>
+    @InjectRepository(Organisation) private organisationRepository: Repository<Organisation>
   ) {}
 
-  async createProduct(orgId: string, dto: CreateProductRequestDto) {
+  async createProduct(id: string, dto: CreateProductRequestDto) {
     const { name, quantity, price } = dto;
-    const org = await this.organisationRepository.findOne({ where: { id: orgId } });
+    const org = await this.organisationRepository.findOne({ where: { id } });
+    if (!org)
+      throw new InternalServerErrorException({
+        status: 'Unprocessable entity exception',
+        message: 'Invalid organisation credentials',
+        status_code: 422,
+      });
     const newProduct: Product = await this.productRepository.create({
       name,
       quantity,
       price,
-      org,
     });
+    newProduct.org = org;
     if (!newProduct)
       throw new InternalServerErrorException({
         status_code: 500,
         status: 'Internal server error',
         message: 'An unexpected error occurred. Please try again later.',
       });
+    const statusCal = await this.calculateProductStatus(quantity);
+    newProduct.satus = statusCal;
     await this.productRepository.save(newProduct);
-    const status = await this.calculateProductStatus(quantity);
     return {
       status: 'success',
       message: 'Product created successfully',
@@ -43,7 +50,7 @@ export class ProductsService {
         name: newProduct.name,
         description: newProduct.description,
         price: newProduct.price,
-        status,
+        status: newProduct.satus,
         quantity,
         created_at: newProduct.created_at,
         updated_at: newProduct.updated_at,
