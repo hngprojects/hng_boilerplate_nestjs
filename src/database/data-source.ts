1f69a1fa7d5de7d5f7cd791988b7d2620b1c7b0f
zee-ham-su <hsufiian@yahoo.com>
2024-07-30 21:28:10 +0000

    chore: Add environment variables for Heroku

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -16,7 +16,7 @@ const dataSource = new DataSource({
   migrations: [process.env.DB_MIGRATIONS],
   synchronize: isDevelopment,
   migrationsTableName: 'migrations',
-  ssl: process.env.DB_SSL === 'true',
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
 });
 
 export async function initializeDataSource() {
