1d291e6ef37d1515e0a526d98c18b7dfe1dc2538
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-07-30 12:55:36 +0000

    feat: matching dto and mapper to the entity

--- src/modules/organisations/dto/add-user-dto.ts
+++ src/modules/organisations/dto/add-user-dto.ts
@@ -1,5 +1,19 @@
 import { IsString } from 'class-validator';
 
+export class AddMemberToOrganisationDto {
+  @IsString()
+  readonly user_id: string;
+
+  @IsString()
+  readonly organisation_id: string;
+
+  @IsString()
+  readonly role: string;
+
+  @IsString()
+  readonly profile_id: string;
+}
+
 export class AddUserToOrganisationDto {
   @IsString()
   readonly userId: string;
---
+++ src/modules/organisations/mapper/add-member-to-org.mapper.ts
@@ -0,0 +1,18 @@
+import { AddMemberToOrganisationDto } from '../dto/add-user-dto';
+import { OrganisationMember } from '../entities/org-members.entity';
+
+export class AddMemberToOrganisationMapper {
+  static mapToEntity(dto: AddMemberToOrganisationDto): OrganisationMember {
+    if (!dto) {
+      throw new Error('AddMemberToOrganisationDto is required');
+    }
+    const orgMember = new OrganisationMember();
+
+    orgMember.user_id = dto.user_id;
+    orgMember.organisation_id = dto.organisation_id;
+    orgMember.profile_id = dto.organisation_id;
+    orgMember.role = dto.role;
+
+    return orgMember;
+  }
+}
