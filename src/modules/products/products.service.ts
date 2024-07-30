ae3774d6028919e2b292d0e939a9e8e3ecaa8407
Amal-Salam <amal_salam@yahoo.com>
2024-07-30 02:33:43 +0100

    fixed code based on the changes to products and org

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,32 +1,3 @@
-// import { Injectable, NotFoundException } from '@nestjs/common';
-// import { InjectRepository } from '@nestjs/typeorm';
-// import { Repository } from 'typeorm';
-// import { Product } from './entities/product.entity';
-// import { ProductCategory } from '../product-category/entities/product-category.entity';
-// import { CreateProductDto } from './dto/create-product.dto';
-
-// @Injectable()
-// export class ProductsService {
-//   constructor(
-//     @InjectRepository(Product)
-//     private productRepository: Repository<Product>,
-
-//     @InjectRepository(ProductCategory)
-//     private categoryRepository: Repository<ProductCategory>
-//   ) {}
-
-//   async create(createProductDto: CreateProductDto): Promise<Product> {
-//     const category = await this.categoryRepository.findOneBy({ id: createProductDto.categoryId });
-//     if (!category) {
-//       throw new NotFoundException(`Category with ID ${createProductDto.categoryId} not found`);
-//     }
-//     const product = this.productRepository.create({
-//       ...createProductDto,
-//       category,
-//     });
-//     return await this.productRepository.save(product);
-//   }
-// }
 import {
   BadRequestException,
   HttpStatus,
