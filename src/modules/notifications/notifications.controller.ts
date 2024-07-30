3763889c1687417cd6e5bd60a9bd1e7c1ccf52a5
zee-ham-su <hsufiian@yahoo.com>
2024-07-30 18:51:32 +0000

    feat: refactor notifications controller

--- src/modules/notifications/notifications.controller.ts
+++ src/modules/notifications/notifications.controller.ts
@@ -5,7 +5,7 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 @ApiBearerAuth()
 @ApiTags('Notifications')
-@Controller('api/v1/notifications')
+@Controller('notifications')
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
 
