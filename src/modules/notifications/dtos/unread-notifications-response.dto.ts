f94bf486a65964ca8f604d03c980427369dab943
zee-ham-su <hsufiian@yahoo.com>
2024-07-31 16:26:54 +0000

    chore: resolved conflicts

--- src/modules/notifications/dtos/unread-notifications-response.dto.ts
+++ src/modules/notifications/dtos/unread-notifications-response.dto.ts
@@ -1,18 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 export class UnreadNotificationsResponseDto {
+  @ApiProperty()
   status: string;
-
-  message: string;
-
+  @ApiProperty()
   status_code: number;
-
-  data: {
-    totalNotificationCount: number;
-    totalUnreadNotificationCount: number;
-    //notifications: {
-    // id: string;
-    //message: string;
-    //is_read: boolean;
-    //created_at: Date;
-    //}[];
-  };
+  @ApiProperty()
+  message: string;
+  @ApiProperty({
+    type: 'object',
+    properties: {
+      notifications: {
+        type: 'array',
+        items: { type: 'string' },
+        example: [
+          {
+            id: 'string',
+            message: 'string',
+            is_read: 'boolean',
+            created_at: Date.now(),
+          },
+        ],
+      },
+    },
+  })
+  data: object;
 }
