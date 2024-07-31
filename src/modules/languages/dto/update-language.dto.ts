c09eecbf76c8144aae43a4b58094d49fe7f9909e
ZEDD2468 <victoradebayo360@gmail.com>
2024-07-31 01:58:33 +0100

    feat(timezones): created timezone endpoint

--- src/modules/languages/dto/update-language.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateLanguageDto } from './create-language.dto';
-
-export class UpdateLanguageDto extends PartialType(CreateLanguageDto) {}
--- src/modules/languages/languages.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LanguagesService } from './languages.service';
-import { LanguagesController } from './languages.controller';
-
-@Module({
-  controllers: [LanguagesController],
-  providers: [LanguagesService],
-})
-export class LanguagesModule {}
