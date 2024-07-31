b3d8131a9c45a53fea85ff659687277fe235dea9
Ismail Akintunde <homohaykins619@gmail.com>
2024-07-31 23:11:21 +0100

    test: testing pipeline fix

--- src/app.e2e.spec.ts
+++ src/app.e2e.spec.ts
@@ -12,7 +12,7 @@ describe('Health Check Test', () => {
 
   describe('Get Health endpoint', () => {
     it('should return healthy endpoint', async () => {
-      const result = { message: 'This is a healthy endpoint', status_code: 200 };
+      const result = { message: 'This is a healthy endpoint ...', status_code: 200 };
 
       expect(await healthController.health()).toStrictEqual(result);
     });
