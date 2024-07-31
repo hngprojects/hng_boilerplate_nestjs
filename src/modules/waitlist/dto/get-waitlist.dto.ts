6ff567ae294c70887d96bd7f0292b430bf863c9b
khayss <forendals@gmail.com>
2024-07-31 23:35:43 +0100

    chore: fix waitlist circular dependency issue

--- src/modules/waitlist/dto/get-waitlist.dto.ts
+++ src/modules/waitlist/dto/get-waitlist.dto.ts
@@ -2,15 +2,17 @@ export class GetWaitlistResponseDto {
   status: number;
   status_code: number;
   message: string;
-  data: {
-    waitlist: {
-      id: string;
-      name: string;
-      email: string;
-      status: boolean;
-      url_slug?: string;
-      createdAt: Date;
-      updatedAt: Date;
-    }[];
-  };
+  data: GetWaitlistDataInterface;
+}
+
+interface GetWaitlistDataInterface {
+  waitlist: {
+    id: string;
+    name: string;
+    email: string;
+    status: boolean;
+    url_slug?: string;
+    createdAt: Date;
+    updatedAt: Date;
+  }[];
 }
