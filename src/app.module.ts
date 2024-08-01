3e84928559340c9e5a37f2bd39809ce661ba1d03
elijah arhinful <elijaharhinful8@gmail.com>
2024-08-01 19:05:06 +0000

    fix: fixed module not found error

--- src/app.module.ts
+++ src/app.module.ts
@@ -38,7 +38,7 @@ import { WaitlistModule } from './modules/waitlist/waitlist.module';
 import { HelpCenterModule } from './modules/help-center/help-center.module';
 import { OrganisationRoleModule } from './modules/organisation-role/organisation-role.module';
 import { FaqModule } from './modules/faq/faq.module';
-import { WaitlistPageModule } from './modules/waitlist-pages/waitlist/waitlist-pages.module';
+import { WaitlistPageModule } from './modules/waitlist-pages/waitlist-pages.module';
 
 @Module({
   providers: [
