7fa62dcbb30ff679be03f9eb13efb1fe9a303986
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2024-08-02 10:01:50 +0100

    chore: add swagger docs to update temmplate dto

--- src/modules/email/dto/update-email-template.dto.ts
+++ src/modules/email/dto/update-email-template.dto.ts
@@ -1,16 +1,20 @@
-import { IsOptional, IsString } from "class-validator";
-import { CreateEmailTemplateDto } from "./create-email-template.dto";
-import { IsHtml } from "../validators/html.validator";
+import { IsOptional, IsString } from 'class-validator';
+import { CreateEmailTemplateDto } from './create-email-template.dto';
+import { IsHtml } from '../validators/html.validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateEmailTemplateDto {
+  @ApiProperty({ description: 'The name if the template', example: 'confirmation' })
   @IsOptional()
   @IsString()
   name: string;
 
+  @ApiProperty({ description: 'The subject of the template', example: 'Boilerplate Welcome Mail' })
   @IsOptional()
   @IsString()
   subject: string;
 
+  @ApiProperty({ description: 'The content of the template', example: '<h1></h1>' })
   @IsOptional()
   @IsString()
   @IsHtml({ message: 'content must be valid html' })
