603a068dfcecb8fb84c6540ca947f227f5607bf5
King-AJr <talk2king.aj@gmail.com>
2024-08-05 21:24:00 +0100

    chore: add super admin guard to template routes

--- src/modules/email/email.controller.ts
+++ src/modules/email/email.controller.ts
@@ -1,39 +1,37 @@
-import { Controller, Post, Get, Body, Res } from '@nestjs/common';
+import { Controller, Post, Get, Body, Res, UseGuards } from '@nestjs/common';
 import { Response } from 'express';
 import { EmailService } from './email.service';
 import { skipAuth } from '../../helpers/skipAuth';
-import { SendEmailDto, createTemplateDto, getTemplateDto } from './dto/email.dto';
+import { createTemplateDto, getTemplateDto } from './dto/email.dto';
 import { skip } from 'node:test';
+import { SuperAdminGuard } from '../../guards/super-admin.guard';
 
 @Controller('email')
 export class EmailController {
   constructor(private emailService: EmailService) {}
 
-  @skipAuth()
-  @Post('send-confirmation-otp')
-  async sendEmailConfirmationOtp(@Body() body: SendEmailDto, @Res() response: Response): Promise<any> {
-    await this.emailService.sendEmail(body);
-    return response.status(200).json({ message: 'Email confirmation OTP sent successfully' });
-  }
-
+  @UseGuards(SuperAdminGuard)
   @Post('store-template')
   async storeTemplate(@Body() body: createTemplateDto, @Res() res: Response): Promise<any> {
     const response = await this.emailService.createTemplate(body);
     res.status(response.status_code).send(response);
   }
 
+  @UseGuards(SuperAdminGuard)
   @Post('get-template')
   async getTemplate(@Body() body: getTemplateDto, @Res() res: Response): Promise<any> {
     const response = await this.emailService.getTemplate(body);
     res.status(response.status_code).send(response);
   }
 
+  @UseGuards(SuperAdminGuard)
   @Post('delete-template')
   async deleteTemplate(@Body() body: getTemplateDto, @Res() res: Response): Promise<any> {
     const response = await this.emailService.deleteTemplate(body);
     res.status(response.status_code).send(response);
   }
 
+  @UseGuards(SuperAdminGuard)
   @Get('get-all-templates')
   async getAllTemplates(@Res() res: Response): Promise<any> {
     const response = await this.emailService.getAllTemplates();
