35ef876753e1a6c7d722e3c93e3574b68e04a15e
Stephen <stevobialor@gmail.com~>
2024-08-05 18:50:09 +0100

    feat: add the reset password endpoint

--- src/modules/auth/dto/updatePasswordDto.ts
+++ src/modules/auth/dto/updatePasswordDto.ts
@@ -1,12 +1,27 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, IsStrongPassword, MinLength } from 'class-validator';
 
 export class UpdatePasswordDto {
   @ApiProperty({
+    description: 'The email address of the user',
+    example: 'john@example.com',
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({
     description: 'The new password of the user',
   })
-  @IsString()
+  @MinLength(8)
   @IsNotEmpty()
+  @IsStrongPassword(
+    {},
+    {
+      message:
+        'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
+    }
+  )
   newPassword: string;
 
   @ApiProperty({
