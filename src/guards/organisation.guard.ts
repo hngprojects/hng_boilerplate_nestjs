2cdd1b32d56f23e2f7ccdee994ebb1d1657a83d5
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2024-08-05 21:47:48 +0100

    feat: add more checks for guard

--- src/guards/organisation.guard.ts
+++ src/guards/organisation.guard.ts
@@ -15,10 +15,28 @@ export class OrganisationGuard implements CanActivate {
     const organisationId = request.params.id;
 
     const { sub } = context.switchToHttp().getRequest().user;
-    const user = await this.userRepository.findOne({ where: { id: sub } });
-    if (user.user_type === UserType.SUPER_ADMIN || UserType.ADMIN) {
+    const user = await this.userRepository.findOne({
+      where: { id: sub },
+      relations: ['owned_organisations', 'created_organisations', 'organisationMembers'],
+    });
+
+    if (!user) {
+      return false;
+    }
+    // check if user is admin or super admin
+    if (user.user_type === UserType.SUPER_ADMIN || user.user_type === UserType.ADMIN) {
       return true;
     }
+
+    // check if user owns the organisation
+    const isOwner = user.owned_organisations.some(org => org.id === organisationId);
+    if (isOwner) return true;
+
+    // check if user is the creator of the organisation
+    const isCreator = user.created_organisations.some(org => org.id === organisationId);
+    if (isCreator) return true;
+
+    // check if user is a member of the organisation
     const isMember = user.organisationMembers.some(org => org.id === organisationId);
     return isMember;
   }
