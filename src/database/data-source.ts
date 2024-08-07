2a5f7539342967abe12fb4936d61b62b94243211
zee-ham-su <hsufiian@yahoo.com>
2024-08-07 18:48:49 +0000

    chore: updated user service

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -16,7 +16,7 @@ const dataSource = new DataSource({
   migrations: [process.env.DB_MIGRATIONS],
   synchronize: isDevelopment,
   migrationsTableName: 'migrations',
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+  ssl: process.env.DB_SSL === 'true',
 });
 export async function initializeDataSource() {
   if (!dataSource.isInitialized) {
