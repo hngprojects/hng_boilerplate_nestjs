85a87298633855e1b926fd96b67b4db2ea4e9525
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2024-08-07 23:51:39 +0100

    feat: create organisation permissions guard

--- src/guards/permission.decorator.ts
+++ src/guards/permission.decorator.ts
@@ -1,4 +1,5 @@
 import { SetMetadata } from '@nestjs/common';
 import { PermissionCategory } from 'src/modules/organisation-permissions/helpers/PermissionCategory';
+
 export const PERMISSIONS_KEY = 'permissions';
 export const Permissions = (...permissions: PermissionCategory[]) => SetMetadata(PERMISSIONS_KEY, permissions);
