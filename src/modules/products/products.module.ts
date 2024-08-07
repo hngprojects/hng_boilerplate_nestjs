e5cf7328c5a6e6ba4ecb580c7496149f160e09fb
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2024-08-07 23:32:03 +0100

    fix: fix merge conflicts

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -5,12 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { Organisation } from '../organisations/entities/organisations.entity';
 import { ProductVariant } from './entities/product-variant.entity';
-import { User } from '../user/entities/user.entity';
 import { OrganisationMember } from '../organisations/entities/org-members.entity';
+import { Comment } from '../comments/entities/comments.entity';
+import { User } from '../user/entities/user.entity';
+import { UserModule } from '../user/user.module';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
-  imports: [TypeOrmModule.forFeature([Product, Organisation, ProductVariant, User, OrganisationMember])],
+  imports: [
+    TypeOrmModule.forFeature([Product, Organisation, ProductVariant, User, OrganisationMember, Comment]),
+    UserModule,
+  ],
 })
 export class ProductsModule {}
