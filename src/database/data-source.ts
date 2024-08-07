e1d168b0119b23c5dc93825c280f36726466d968
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2024-08-07 23:16:18 +0100

    revert: revert changes to datasource

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -17,11 +17,6 @@ const dataSource = new DataSource({
   synchronize: isDevelopment,
   migrationsTableName: 'migrations',
   ssl: process.env.DB_SSL === 'true',
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
 });
 export async function initializeDataSource() {
   if (!dataSource.isInitialized) {
