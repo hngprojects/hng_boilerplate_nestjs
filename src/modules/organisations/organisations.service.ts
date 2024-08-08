67b88888441c770788b4bdd08c46ba678e2af177
Ismail Akintunde <homohaykins619@gmail.com>
2024-08-08 18:27:29 +0100

    feat: fixed issue with guard

--- src/modules/organisations/organisations.service.ts
+++ src/modules/organisations/organisations.service.ts
@@ -74,7 +74,7 @@ export class OrganisationsService {
       });
 
       const superAdminRole = await this.roleRepository.findOne({ where: { name: 'super-admin' } });
-      console.log(superAdminRole);
+
       const organisationInstance = new Organisation();
       Object.assign(organisationInstance, createOrganisationDto);
       organisationInstance.owner = owner;
