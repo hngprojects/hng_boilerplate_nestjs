8d9e521f7119e54ee8be150f697c27bf42dbf7b1
King-Mikaelson <anazodomichael27@gmail.com>
2024-08-11 10:55:06 +0100

    feat(profile-pic): implemented change profile picture

--- src/modules/profile/dto/file.validator.ts
+++ src/modules/profile/dto/file.validator.ts
@@ -1,4 +1,5 @@
-import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import { PipeTransform, Injectable, ArgumentMetadata, HttpStatus } from '@nestjs/common';
+import { CustomHttpException } from '../../../helpers/custom-http-filter';
 
 @Injectable()
 export class FileValidator implements PipeTransform {
@@ -6,15 +7,21 @@ export class FileValidator implements PipeTransform {
 
   transform(value: Express.Multer.File, metadata: ArgumentMetadata) {
     if (!value) {
-      throw new BadRequestException('No file provided');
+      throw new CustomHttpException('No file provided', HttpStatus.BAD_REQUEST);
     }
 
     if (value.size > this.options.maxSize) {
-      throw new BadRequestException(`File size exceeds ${this.options.maxSize / (1024 * 1024)}MB limit`);
+      throw new CustomHttpException(
+        `File size exceeds ${this.options.maxSize / (1024 * 1024)}MB limit`,
+        HttpStatus.BAD_REQUEST
+      );
     }
 
     if (!this.options.mimeTypes.includes(value.mimetype)) {
-      throw new BadRequestException(`Invalid file type. Allowed types: ${this.options.mimeTypes.join(', ')}`);
+      throw new CustomHttpException(
+        `Invalid file type. Allowed types: ${this.options.mimeTypes.join(', ')}`,
+        HttpStatus.BAD_REQUEST
+      );
     }
 
     return value;
