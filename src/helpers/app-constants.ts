819eb2796ae0f835ce07cdbb91c2c65b9f8fd85c
King-Mikaelson <anazodomichael27@gmail.com>
2024-08-12 07:29:47 +0100

    feat(profile-pic): implemented change profile picture

--- src/helpers/app-constants.ts
+++ src/helpers/app-constants.ts
@@ -2,6 +2,5 @@ import * as path from 'path';
 
 export const MAX_PROFILE_PICTURE_SIZE =  2 * 1024 * 1024;
 export const VALID_UPLOADS_MIME_TYPES = ['image/jpeg', 'image/png'];
-// export const BASE_URL = "https://staging.api-nestjs.boilerplate.hng.tech";
-export const BASE_URL = "http://localhost:3008";
+export const BASE_URL = "https://staging.api-nestjs.boilerplate.hng.tech";
 export const PROFILE_PHOTO_UPLOADS =  path.join(__dirname, '..',  'uploads')
