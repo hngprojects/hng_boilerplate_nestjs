bb7db8736bd3730d0faadd26c9fb5143c9be2aa5
Ismail Akintunde <homohaykins619@gmail.com>
2024-08-24 01:47:51 +0100

    feat: updated google authentication logic to fix inconsistencies

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -287,12 +287,8 @@ export default class AuthenticationService {
     const idToken = googleAuthPayload.id_token;
     let verifyTokenResponse: GoogleVerificationPayloadInterface;
 
-    if (isMobile === 'true') {
-      const request = await fetch(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${idToken}`);
-      verifyTokenResponse = await request.json();
-    } else {
-      verifyTokenResponse = await this.googleAuthService.verifyToken(idToken);
-    }
+    const request = await fetch(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${idToken}`);
+    verifyTokenResponse = await request.json();
 
     const userEmail = verifyTokenResponse.email;
     const userExists = await this.userService.getUserRecord({ identifier: userEmail, identifierType: 'email' });
