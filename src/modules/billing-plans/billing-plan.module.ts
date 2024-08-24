73fe3806c731e6c0d90e0fe305e4fc14db06dc2c
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-08-24 13:10:32 +0300

    fix: adding dependencies

--- src/modules/billing-plans/billing-plan.module.ts
+++ src/modules/billing-plans/billing-plan.module.ts
@@ -4,9 +4,12 @@ import { BillingPlanController } from './billing-plan.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BillingPlan } from './entities/billing-plan.entity';
 import { User } from '../user/entities/user.entity';
+import { Organisation } from '../organisations/entities/organisations.entity';
+import { OrganisationUserRole } from '../role/entities/organisation-user-role.entity';
+import { Role } from '../role/entities/role.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BillingPlan, User])],
+  imports: [TypeOrmModule.forFeature([BillingPlan, User, Organisation, OrganisationUserRole, Role])],
   controllers: [BillingPlanController],
   providers: [BillingPlanService],
 })
