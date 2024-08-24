5fe232fe56fbe5e4c29941c273a232027e27b202
olamstevy <olamstevy@gmail.com>
2024-08-24 12:16:41 +0100

    fix: refactored code and included superadmin guards when getting waiting list

--- src/modules/waitlist/waitlist.module.ts
+++ src/modules/waitlist/waitlist.module.ts
@@ -3,10 +3,14 @@ import { WaitlistController } from './waitlist.controller';
 import WaitlistService from './waitlist.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Waitlist } from './entities/waitlist.entity';
+import { User } from '../user/entities/user.entity';
+import { OrganisationUserRole } from '../role/entities/organisation-user-role.entity';
+import { Organisation } from '../organisations/entities/organisations.entity';
+import { Role } from '../role/entities/role.entity';
 
 @Module({
   controllers: [WaitlistController],
   providers: [WaitlistService],
-  imports: [TypeOrmModule.forFeature([Waitlist])],
+  imports: [TypeOrmModule.forFeature([Waitlist, User, OrganisationUserRole, Organisation, Role])],
 })
 export class WaitlistModule {}
