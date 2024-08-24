7c3170f8764097dc4b3ff21cfdcb8d49e1d197bf
ObodoakorDavid <obodoakordavid@outlook.com>
2024-08-24 10:15:02 +0100

    fix: fix import path for skipAuth

--- src/modules/api-status/api-status.controller.ts
+++ src/modules/api-status/api-status.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { ApiStatusService } from './api-status.service';
 import { CreateApiStatusDto } from './dto/create-api-status.dto';
-import { skipAuth } from 'src/helpers/skipAuth';
+import { skipAuth } from '../../helpers/skipAuth';
 
 @Controller('api-status')
 export class ApiStatusController {
