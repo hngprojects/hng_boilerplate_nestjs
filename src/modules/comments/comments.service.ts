d8b94d993d531df56cc0751dd9897a15640b2008
Oluwaseyi Idowu <idowuseyi22@gmail.com>
2024-08-24 15:24:00 +0100

    fix: reduce data fetching for performance

--- src/modules/comments/comments.service.ts
+++ src/modules/comments/comments.service.ts
@@ -43,11 +43,7 @@ export class CommentsService {
     };
   }
 
-  async getAComment(commentId: string, userId: string) {
-    const user = await this.userRepository.findOne({ where: { id: userId } });
-    if (!user) {
-      throw new CustomHttpException('User not found', HttpStatus.NOT_FOUND);
-    }
+  async getAComment(commentId: string) {
     const comment = await this.commentRepository.findOneBy({ id: commentId });
     if (!comment) {
       throw new CustomHttpException('Comment not found', HttpStatus.NOT_FOUND);
