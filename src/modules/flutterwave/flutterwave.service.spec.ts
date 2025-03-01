6a38a8ab157cab1d7f257665fcbc929002ff8843
urizennnn <igamerryt@gmail.com>
2025-03-01 22:04:10 +0100

    fix: added axios to flutterwave spec file

--- src/modules/flutterwave/flutterwave.service.spec.ts
+++ src/modules/flutterwave/flutterwave.service.spec.ts
@@ -30,7 +30,7 @@ describe('FlutterwaveService', () => {
         {
           provide: ConfigService,
           useValue: {
-            get: jest.fn().mockReturnValue('mock-base-url'),
+            get: jest.fn().mockReturnValue('http://mock-base-url'),
           },
         },
         {
